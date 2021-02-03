f8a89532299089006c65eef17142a513ba4219ca
Arnob Chakma <arnobchakma05@gmail.com>
2021-02-03 00:16:25 -0800

    null vs undefined

---
+++ SecondNullVsUndefined.js
@@ -0,0 +1,34 @@
+//Kokon kokon result undefined asbe /undefined asar karon
+let white; //First Jodi value declare na koro;
+console.log(white);
+
+
+//Second Jodi function ke return na koro
+function added(num1, num2) {
+    const sum = num1 + num2;
+}
+const result = added(22, 66);
+console.log(result)
+
+
+//Third Jodi duita/tinta/sarta parameter likhe matro ekta value set koro
+function added(num1, num2) {
+    const sum = num1 + num2;
+    return sum;
+}
+const result = added(22);
+console.log(result)
+
+
+//Forth Jodi object er moddhe thaka property tumi declare na koro.
+const friend = {
+    Name: 'Beauty',
+    Phone: 223757
+};
+const result = friend.gf;
+console.log(result);
+
+
+//fifth array moddhe thaka songka declare na kora
+const friend = [4, 5, 6, 7];
+console.log(friend[11])
