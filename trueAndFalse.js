50d5d9edf2ee1a78b2440b09131636a074839b63
Arnob Chakma <arnobchakma05@gmail.com>
2021-02-02 23:15:54 -0800

    true false condition

---
+++ trueAndFalse.js
@@ -0,0 +1,21 @@
+let myName = 'Henry';
+if(myName.length > 0){
+    console.log('This condition is true')
+}
+else{
+    console.log('This condition is false')
+}
+//False Condition
+//0     Number er ketre false hocche 0; baki sobgulo ture hobe
+//""    String er ketre false tokon hobe jokon empty string take.
+//NaN   NaN takle dorte hobe eita false hobe
+//undefined     undefined takle condition hobe false 
+//null      Null takleo condition hobe false 
+//false     Condition false hole obossoi eita false hobe
+
+//Truthy
+//" "   Empty string er moddhe ekta white space takle o oita ture hicebe dore nei
+//[]    value modhhe empty array takleo ta ture hichebe dore nei
+//{}    value moddhe empty second bracket takleo ta ture hichebe dore nei
+//10    Jekono purno songa holei ta ture hobe.
+//"ABC"     String er moddhe jai lekha takuk na keno oita true hobe
